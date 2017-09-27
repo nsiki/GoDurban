26db3750757a89f8937480f99f5ca7533c1d9820
nsiki <nsikimdlalose1@webmail.co.za>
2017-09-28 01:43:18 +0200

    the signup form

---
+++ SignUp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ <h3><image src="GoDurban.png" style="width:700px;height:400px;"></h3>
+  <h1>Sign Up to Ride</h1>
+  <h2>Always there on time</h2>
+   <form>
+      <input type="text" placeholder="First Name"><br>
+	  <input type="text" placeholder="Last Name"><br>
+	  <input type="number" placeholder="cellphone number"><br>
+	  <input type="email" placeholder="Email"><br>
+	  <input type="password" placeholder="Password"><br>
+	  <input type="password" placeholder="Confirm Password"><br>
+	  
+	  <input type="submit" name="Sign Up ">
+   </form>
+
+</body>
+<html>
